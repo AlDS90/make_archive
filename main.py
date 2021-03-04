2193af626a31d1e558ea3a330ee43a36b43fe7f6
AlDS90 <lkjs0JH)0fw3vcSFDSF9>
2021-03-04 12:57:14 +0700

    finish

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import zipfile
+import os
 
 
 def zip_(path: str, z_path: str):
     z_file = zipfile.ZipFile(z_path, 'w')
-    z_file.write(path)
+    if os.path.isfile(path):
+        z_file.write(path)
+    else:
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                z_file.write(root + os.sep + file)
     z_file.close()
 
 
-zip_('1.txt', 'archive.zip')
+zip_('archive_folder', 'archive.zip')
